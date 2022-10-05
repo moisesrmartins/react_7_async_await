5bb80445ceee01cba740b8916fc99b5b7b1f2357
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-05 17:04:59 -0300

    React_7_Async_Await Parte 1.2: setTimeout

--- async/src/App.js
+++ async/src/App.js
@@ -4,7 +4,15 @@ function App() {
   useEffect(() => {
     console.log(1);
   });
-  alert(1);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeout(() => {
+        console.log("hi");
+      }, 1000);
+      console.log("hello");
+    }, 2000);
+  });
 
   return <div className="App"></div>;
 }
