dc6786867215eca49a2ec7e13c9f0ec17f65a8da
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-05 18:41:19 -0300

    React_7_Async_Await Parte 1.2: setPromise

--- async/src/App.js
+++ async/src/App.js
@@ -1,5 +1,12 @@
 import React, { useEffect } from "react";
 
+const setPromise = (time, text) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(text);
+    }, time);
+  });
+
 function App() {
   useEffect(() => {
     console.log(1);
@@ -13,6 +20,12 @@ function App() {
       console.log("hello");
     }, 2000);
   });
+
+  setPromise(2000, "good morning")
+    .then(() => setPromise(1000, "have"))
+    .then(() => setPromise(1000, "a"))
+    .then(() => setPromise(1000, "nice"))
+    .then(() => setPromise(1000, "day"));
 
   return <div className="App"></div>;
 }
